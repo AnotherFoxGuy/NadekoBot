80a41c1f38a9299e5fc3cf730a583b8e4ffadc98
Kwoth <master.kwoth@gmail.com>
2024-11-17 19:14:40 +0000

    fix: fixed nullref in blacklist

--- src/NadekoBot/Modules/Searches/_common/StreamNotifications/Providers/StreamProvider.cs
+++ src/NadekoBot/Modules/Searches/_common/StreamNotifications/Providers/StreamProvider.cs
@@ -5,7 +5,7 @@ namespace NadekoBot.Modules.Searches.Common.StreamNotifications.Providers;
 /// <summary>
 ///     Abstract class implemented by providers of all supported platforms
 /// </summary>
-public abstract class Provider
+public abstract class StreamProvider
 {
     /// <summary>
     ///     Type of the platform.
